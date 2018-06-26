065a9f08cba63605b78d8ba4b6f49072b46775c5
Chronial <git@chronial.de>
2018-06-26 18:28:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     description=(
         'A flake8 plugin to check for use of division without a '
         '__future__.division import'),
+    url='https://github.com/Chronial/flake8-future-division',
+    author='Christian Fersch',
+    license='MIT',
     version='0.1',
     py_modules=['flake8_division'],
     entry_points={'flake8.extension': ['F48 = flake8_division:CheckDivision']},
